84769441c754fedf6c5f4b0a545f45879b1ea8e3
Avinash217 <55040298+Avinash217@users.noreply.github.com>
2020-10-21 01:11:13 +0530

    Update providers.tf

--- RDS/providers.tf
+++ RDS/providers.tf
@@ -1,7 +1,7 @@
 #Providers
 
 provider "aws" {
-access_key = "AKIAYXGJH2RU753HIELH"
-secret_key = "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"
+access_key = "XGJH2RU753HIELH"
+secret_key = "tFnbFgP7iTxEr3sZIKY0iuZy0/fO2cZBg9"
 region = "us-east-1"
 }
